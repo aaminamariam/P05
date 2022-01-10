020fbd4bc49a5e9a73a37fd1dc14736fc0c1397a
Mohammad Yousuf <22100289@lums.edu.pk>
2022-01-10 16:40:02 +0500

    fixed active error in addreq api

--- P05-HR Management System/Development/Sprint-1/server.js
+++ P05-HR Management System/Development/Sprint-1/server.js
@@ -17,7 +17,7 @@ app.use(
   cors({
     origin: "*",
   })
-)
+);
 
 app.use(express.json());
 
@@ -62,16 +62,14 @@ app.delete("/ids/:id", async (req, res) => {
 
 //add request
 app.post("/addreq", async (req, res) => {
-  
   const data = req.body;
-
   console.log(data);
   try {
     const newCharacter = await addrequest(
       data.option,
       data.description,
       data.employeeID,
-      data.status
+      "active"
     );
     res.json(newCharacter);
   } catch (err) {
@@ -81,46 +79,39 @@ app.post("/addreq", async (req, res) => {
 });
 
 //get requests
-app.get("/getrequests", async (req, res) =>
-{
-  try{
+app.get("/getrequests", async (req, res) => {
+  try {
     const requests = await getEmployeeRequests();
-    res.json(requests);    
-  }
-  catch(err) {
+    res.json(requests);
+  } catch (err) {
     console.error(err);
     res.status(500).json({ err: "Something went wrong" });
   }
 });
 
-app.get("/getrequests/:id", async (req, res) =>
-{
+app.get("/getrequests/:id", async (req, res) => {
   const { id } = req.params;
-  try{
+  try {
     // const requests = await getEmployeeReqbyID(id, );
-    res.json( await getEmployeeReqbyID(id, "active"));    
-  }
-  catch(err) {
+    res.json(await getEmployeeReqbyID(id, "active"));
+  } catch (err) {
     console.error(err);
     res.status(500).json({ err: "Something went wrong" });
   }
 });
 
-app.get("/activereq/:id", async (req, res) =>
-{
-  const {id}  = req.params;
+app.get("/activereq/:id", async (req, res) => {
+  const { id } = req.params;
   const rstatus = "active";
   console.log(rstatus);
-  try{
+  try {
     // const requests = await getEmployeeReqbyID(id, );
-    res.json( await getEmployeeReqbystatus(id ,rstatus));    
-  }
-  catch(err) {
+    res.json(await getEmployeeReqbystatus(id, rstatus));
+  } catch (err) {
     console.error(err);
     res.status(500).json({ err: "Something went wrong" });
   }
 });
-
 
 //approve/deny requests
 // app.put("/getrequests", async (req, res) =>
@@ -130,14 +121,13 @@ app.get("/activereq/:id", async (req, res) =>
 //     const requestStatus = await approvedenyRequests(
 //       data.reqStatus,
 //     );
-//     res.json(requestStatus);    
+//     res.json(requestStatus);
 //   }
 //   catch(err) {
 //     console.error(err);
 //     res.status(500).json({ err: "Something went wrong" });
 //   }
 // });
-
 
 // start the server in the port 5000!
 app.listen(process.env.PORT || 5000, function () {
@@ -147,5 +137,5 @@ app.listen(process.env.PORT || 5000, function () {
 app.use(function (req, res, next) {
   res.status(404).send("Sorry, that route doesn't exist. Have a nice day :)");
   res.header("Access-Control-Allow-Origin", "*");
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
 });
