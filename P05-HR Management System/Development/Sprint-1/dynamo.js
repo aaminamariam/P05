807266aa64b2268651ff0da1534eb46485629ed3
squarif <aliadnanarif25@gmail.com>
2022-02-18 02:20:08 +0500

    Add employee working on new table

--- P05-HR Management System/Development/Sprint-1/dynamo.js
+++ P05-HR Management System/Development/Sprint-1/dynamo.js
@@ -11,7 +11,7 @@ AWS.config.update({
 
 const dynamoClient = new AWS.DynamoDB.DocumentClient();
 // const TABLE_NAME = "employee_table";
-const EMPLOYEE_TABLE = "employee_table";
+const EMPLOYEE_TABLE = "employee_directory";
 const REQUESTS_TABLE = "requests_table";
 const ANNOUNCEMENTS_TABLE = "announcements_table";
 
@@ -40,7 +40,7 @@ const addNewEmployee = async (_id, _name, _department, _designation, _level, _da
   const params = {
     TableName: EMPLOYEE_TABLE,
     Item: {
-      employeeID: _id, 
+      id: _id, 
       name: _name, 
       department: _department, 
       designation: _designation, 
