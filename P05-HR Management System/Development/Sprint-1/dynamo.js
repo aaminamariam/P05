caa92cfc835e6b063f976d9e684ff3e4c9021521
Mohammad Yousuf <22100289@lums.edu.pk>
2022-04-03 23:58:00 +0500

    Co-authored-by: Aamina Mariam <aaminamariam@users.noreply.github.com>

--- P05-HR Management System/Development/Sprint-1/dynamo.js
+++ P05-HR Management System/Development/Sprint-1/dynamo.js
@@ -39,21 +39,20 @@ const addrequest = async (type, data, id, title, date) => {
     TableName: REQUESTS_TABLE,
     Key: { id: id },
     UpdateExpression:
-      "SET #type = :type , #data = :data, #status = :status, #title = :title, #dateAdded= :date" ,
+      "SET #type = :type , #data = :data, #status = :status, #title = :title, #dateAdded= :date",
     ExpressionAttributeNames: {
       "#type": "type",
       "#data": "data",
       "#title": "title",
       "#status": "status",
-      "#dateAdded":"dateAdded"
+      "#dateAdded": "dateAdded",
     },
     ExpressionAttributeValues: {
       ":type": type,
       ":data": data,
       ":title": title,
       ":status": "New",
-      ":date": date
-
+      ":date": date,
     },
   };
   return await dynamoClient.update(params).promise();
@@ -64,15 +63,14 @@ const approveRequests = async (id) => {
   const params = {
     TableName: REQUESTS_TABLE,
     Key: { id: id },
-    UpdateExpression:
-      "SET #status = :status",
+    UpdateExpression: "SET #status = :status",
     ExpressionAttributeNames: {
       "#status": "status",
     },
     ExpressionAttributeValues: {
       ":status": "Approved",
     },
- }
+  };
   // dynamoClient.batchWrite(params, function(err, data) {
   //   if (err) console.log(err, err.stack); // an error occurred
   //   else console.log(data);// successful response
@@ -86,8 +84,7 @@ const denyRequests = async (id) => {
   const params = {
     TableName: REQUESTS_TABLE,
     Key: { id: id },
-    UpdateExpression:
-      "SET #status = :status",
+    UpdateExpression: "SET #status = :status",
     ExpressionAttributeNames: {
       "#status": "status",
     },
@@ -103,27 +100,38 @@ const denyRequests = async (id) => {
   return await dynamoClient.update(params).promise();
 };
 
-
 //add new employee
-const addNewEmployee = async (_id, _name, _department, _designation, _level, _dateJoined, _email, _contact, _address, _remainingLeaves, _twRating) =>{
+const addNewEmployee = async (
+  _id,
+  _name,
+  _department,
+  _designation,
+  _level,
+  _dateJoined,
+  _email,
+  _contact,
+  _address,
+  _remainingLeaves,
+  _twRating
+) => {
   const params = {
     TableName: EMPLOYEE_TABLE,
     Item: {
-      id: _id, 
-      name: _name, 
-      department: _department, 
-      designation: _designation, 
-      level: _level, 
-      dateJoined: _dateJoined, 
-      email: _email, 
-      contact: _contact, 
-      address: _address, 
-      remainingLeaves: _remainingLeaves, 
+      id: _id,
+      name: _name,
+      department: _department,
+      designation: _designation,
+      level: _level,
+      dateJoined: _dateJoined,
+      email: _email,
+      contact: _contact,
+      address: _address,
+      remainingLeaves: _remainingLeaves,
       twRating: _twRating,
-    }
-  }
+    },
+  };
   return await dynamoClient.put(params).promise();
-}
+};
 
 // add to database
 const addOrUpdateEmployee = async (employee, name) => {
@@ -199,40 +207,45 @@ const deleteEmployee = async (employeeID) => {
 
 //add employee statics
 const addstats = async (employeeID, comments, rating, teamworkScore, hours) => {
+  let now = new Date();
   const params = {
     TableName: EMPLOYEE_TABLE,
     Key: { employeeID: employeeID },
     UpdateExpression:
-      "SET #comments = :vals , #rating = :rating, #teamscore = :tscore, #hoursworked = :hours",
+      "SET #comments = :vals , #rating = :rating, #teamscore = :tscore, #hoursworked = :hours, #lastupdated = :lastupdated",
     ExpressionAttributeNames: {
       "#comments": "comments",
       "#rating": "rating",
       "#teamscore": "teamworkScore",
       "#hoursworked": "hoursworked",
+      "#lastupdated": "lastupdated",
     },
     ExpressionAttributeValues: {
       ":vals": [comments],
       ":rating": [rating],
       ":tscore": [teamworkScore],
       ":hours": [hours],
+      ":lastupdated": [now],
     },
   };
   const params2 = {
     TableName: EMPLOYEE_TABLE,
     Key: { employeeID: employeeID },
     UpdateExpression:
-      "SET #com = list_append(#com,:vals), #rating = list_append(#rating,:rating), #teamscore = list_append(#teamscore,:tscore),#hoursworked = list_append(#hoursworked,:hours)",
+      "SET #com = list_append(#com,:vals), #rating = list_append(#rating,:rating), #teamscore = list_append(#teamscore,:tscore),#hoursworked = list_append(#hoursworked,:hours), #lastupdated = list_append(:lastupdated)",
     ExpressionAttributeNames: {
       "#com": "comments",
       "#rating": "rating",
       "#teamscore": "teamworkScore",
       "#hoursworked": "hoursworked",
+      "#lastupdated": "lastupdated",
     },
     ExpressionAttributeValues: {
       ":vals": [comments],
       ":rating": [rating],
       ":tscore": [teamworkScore],
       ":hours": [hours],
+      ":lastupdated": [now],
     },
   };
   try {
@@ -249,14 +262,16 @@ const addstats = async (employeeID, comments, rating, teamworkScore, hours) => {
 const getEmployeeStatsbyID = async (id) => {
   const params = {
     TableName: EMPLOYEE_TABLE,
-    ProjectionExpression: "#id, #Rating, #teamscore, #hoursWorked, #comments",
-    KeyConditionExpression: "#ids = :id",
+    ProjectionExpression:
+      "#id, #Rating, #teamscore, #hoursWorked, #comments, #designation",
+    KeyConditionExpression: "#id = :id",
     ExpressionAttributeNames: {
-      "#ids": "employeeID",
+      "#id": "employeeID",
       "#Rating": "rating",
-      "#teamscore": "teamworkScore",
+      "#teamscore": "twRating",
       "#hoursWorked": "hoursworked",
       "#comments": "comments",
+      "#designation": "designation",
     },
     ExpressionAttributeValues: {
       ":id": id,
@@ -266,71 +281,32 @@ const getEmployeeStatsbyID = async (id) => {
   return statsbyID;
 };
 
-// adding announcements
-// const addAnnouncements = async (employeeID, title, aData, date) => {
-//   const params = {
-//     TableName: ANNOUNCEMENTS_TABLE,
-//     Key: { announcement_id: announcement_id },
-//     UpdateExpression: "SET #aData = :vals, #aDatePosted = :dt, #title  = :tit",
-//     ExpressionAttributeNames: {
-//       "#aData": "announcements",
-//       "#aDatePosted": "date",
-//       "#title": "title",
-//     },
-//     ExpressionAttributeValues: {
-//       ":vals": [aData],
-//       ":dt": [dateposted],
-//       ":tit": [title],
-//     },
-//   };
-//   const params2 = {
-//     TableName: ANNOUNCEMENTS_TABLE,
-//     Key: { announcement_id: announcement_id },
-//     UpdateExpression:
-//       "SET #aData = list_append(#aData,:vals), #aDatePosted = list_append(#aDatePosted,:dt), #title  = list_append(#title,:tit)",
-//     ExpressionAttributeNames: {
-//       "#aData": "announcements",
-//       "#aDatePosted": "dateposted",
-//       "#title": "title",
-//     },
-//     ExpressionAttributeValues: {
-//       ":vals": [aData],
-//       ":dt": [dateposted],
-//       ":tit": [title],
-//     },
-//   };
-//   try {
-//     c1 = await dynamoClient.update(params2).promise();
-//   } catch (err) {
-//     try {
-//       c2 = await dynamoClient.update(params).promise();
-//     } catch (err2) {
-//       console.log(err2);
-//     }
-//   }
-// };
-
-const addNewAnnouncement = async (id, postedBy, title, department, data, datePosted) => {
-  
+const addNewAnnouncement = async (
+  id,
+  postedBy,
+  title,
+  department,
+  data,
+  datePosted
+) => {
   const params = {
     TableName: ANNOUNCEMENTS_TABLE,
     Item: {
-      id: id, 
+      id: id,
       title: title,
       postedBy: postedBy,
       department: department,
       data: data,
       datePosted: datePosted,
-    }
-  }
-  console.log("Dynamo recieved: ", params.Item)
-  try{
+    },
+  };
+  console.log("Dynamo recieved: ", params.Item);
+  try {
     newAnnouncement = await dynamoClient.put(params).promise();
+  } catch (error) {
+    console.error(error);
   }
-  catch (error){
-    console.error(error)
-  }
- 
+
   return newAnnouncement;
 };
 
@@ -369,13 +345,9 @@ module.exports = {
   getEmployeeRequests,
   getEmployeeReqbyID,
   getEmployeeReqbystatus,
-  
-  
+
   addstats,
   getEmployeeStatsbyID,
-  // addAnnouncements,
   addNewAnnouncement,
   getAnnouncements,
 };
-
-
